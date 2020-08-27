4e658bb01b42ab047cfd4c4e9d73d007122d2107
Alex <ayagudza@gmail.com>
2020-08-28 00:31:06 +0300

    add localization for UIDashboard

--- plugins/org.jkiss.dbeaver.ui.dashboard/src/org/jkiss/dbeaver/ui/dashboard/internal/UIDashboardMessages.java
+++ plugins/org.jkiss.dbeaver.ui.dashboard/src/org/jkiss/dbeaver/ui/dashboard/internal/UIDashboardMessages.java
@@ -23,8 +23,67 @@ public class UIDashboardMessages extends NLS {
 
     public static String pref_page_dashboards_group_common;
     public static String pref_page_dashboards_open_separate_connection_label;
-
-
+    
+    public static String add_dialog_dashboards_message_no_more_dashboards;
+    public static String add_dialog_dashboards_button_manage;
+    public static String add_dialog_dashboards_button_add;
+    public static String add_dialog_dashboards_title;
+    public static String add_dialog_dashboards_table_name;
+    public static String add_dialog_dashboards_table_description;
+    
+    // Dashboards Edit Dialog
+    public static String edit_dialog_dashboards_title;
+    public static String edit_dialog_dashboards_bracket;
+    public static String edit_dialog_dashboards_group_main;
+    public static String edit_dialog_dashboards_group_queries;
+    public static String edit_dialog_dashboards_group_rendering;
+    public static String edit_dialog_dashboards_label_info_queries;
+    public static String edit_dialog_dashboards_label_info_main;
+    public static String edit_dialog_dashboards_label_ID;
+    public static String edit_dialog_dashboards_label_name;
+    public static String edit_dialog_dashboards_label_database;
+    public static String edit_dialog_dashboards_label_data_type;
+    public static String edit_dialog_dashboards_label_value_type;
+    public static String edit_dialog_dashboards_label_fetch_type;
+    public static String edit_dialog_dashboards_label_calc_type;
+    public static String edit_dialog_dashboards_label_interval;
+    public static String edit_dialog_dashboards_label_description;
+    public static String edit_dialog_dashboards_label_view;
+    public static String edit_dialog_dashboards_label_update_period;
+    public static String edit_dialog_dashboards_label_maximum_items;
+    public static String edit_dialog_dashboards_label_tip_data_type;
+    public static String edit_dialog_dashboards_label_tip_value_type;
+    public static String edit_dialog_dashboards_label_tip_fetch_type;
+    public static String edit_dialog_dashboards_label_tip_calc_type;
+    public static String edit_dialog_dashboards_label_tip_interval;
+    public static String edit_dialog_dashboards_label_tip_view;
+    public static String edit_dialog_dashboards_descriptor_keys;
+    public static String edit_dialog_dashboards_descriptor_labels;
+    public static String edit_dialog_dashboards_button_select;
+    
+    public static String config_dialog_dashboards_title;
+    public static String config_dialog_dashboards_bracket;
+    public static String config_dialog_dashboards_group_info;
+    public static String config_dialog_dashboards_group_update;
+    public static String config_dialog_dashboards_group_view;
+    public static String config_dialog_dashboards_label_name;
+    public static String config_dialog_dashboards_label_description;
+    public static String config_dialog_dashboards_label_update;
+    public static String config_dialog_dashboards_label_items;
+    public static String config_dialog_dashboards_label_view;
+    public static String config_dialog_dashboards_label_legend;
+    public static String config_dialog_dashboards_label_grid;
+    public static String config_dialog_dashboards_label_domain;
+    public static String config_dialog_dashboards_label_range;
+    public static String config_dialog_dashboards_label_tip_view;
+    public static String config_dialog_dashboards_label_tip_legend;
+    public static String config_dialog_dashboards_label_tip_grid;
+    public static String config_dialog_dashboards_label_tip_domain;
+    public static String config_dialog_dashboards_label_tip_range;
+    public static String config_dialog_dashboards_button_configuration;
+    public static String config_dialog_dashboards_button_queries;
+    public static String config_dialog_dashboards_context;
+    
     static {
         // initialize resource bundle
         NLS.initializeMessages(BUNDLE_NAME, UIDashboardMessages.class);
